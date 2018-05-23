df8e6827d2fc8b596cadafb7a8385c0ce398c84c
NikkiBytes <nicholletteacosta@gmail.com>
2018-05-22 23:25:49 -0400

    intial start

---
+++ preproc.py
@@ -0,0 +1,14 @@
+# practice for work, lesson scripts from fMRI workshop 2017
+import glob
+import os
+import pdb
+import subprocess
+import argparse
+import datetime
+import shutil
+
+def preproc(basedir):
+    print("We are in the base directory")
+def main():
+    basedir='/User/nikkibytes/'
+    preproc(basedir)
